4c4f9a9a6138cfc13c788f0d205e8a1cdde048e1
sujithcp <sooji.sujith@gmail.com>
2016-09-19 00:14:46 +0530

    update

--- FaceRecognizer-C/Generals.cpp
+++ FaceRecognizer-C/Generals.cpp
@@ -2,15 +2,17 @@
 // Created by sujith on 13/09/16.
 //
 #include <iostream>
-#include "Generals.h"
 #include <fstream>
-#include "opencv2/face.hpp"
-
-
-using namespace cv;
+#include <math.h>
+#include <dlib/image_processing.h>
+#include <dlib/image_io.h>
+#include <dlib/image_processing/frontal_face_detector.h>
+#include <dlib/opencv/cv_image.h>
+#include <opencv2/face.hpp>
+#include "Generals.h"
 using namespace cv::face;
-using namespace std;
 CascadeClassifier face_detect;
+CascadeClassifier eye_detect;
 Ptr<FaceRecognizer> model = createLBPHFaceRecognizer();
 vector<Mat> getFacesFromImage(Mat image)
 {
@@ -24,8 +26,8 @@ vector<Mat> getFacesFromImage(Mat image)
         cout<<x<<" "<<y<<" "<<w<<" "<<h<<endl;
         imshow("FACE",Mat(image,Rect(x,y,w,h)));
         waitKey(1000);
-        Mat tmpFace = Mat(image,Rect(x,y,w,h));
-        if(tmpFace.rows<250 || tmpFace.cols<250)
+        Mat tmpFace = preProcess(Mat(image,Rect(x,y,w,h)));
+        if(tmpFace.rows<100 || tmpFace.cols<100)
             continue;
         face_imgs.push_back(tmpFace);
     }
@@ -72,6 +74,17 @@ void train()
     vector<int> labels;
     prepareTrainData("/home/sujith/ClionProjects/Comma/res/images/OUT/faces_list.txt",images,labels,' ');
     model->train(images,labels);
+    model->save("/home/sujith/ClionProjects/Comma/MODEL.YAML");
+}
+
+int recognize(Mat image)
+{
+    cvtColor(image,image,COLOR_BGR2GRAY);
+    int res=-1;
+    double conf=-1;
+    model->predict(image, res, conf);
+    cout<<res<<" "<<conf<<endl;
+    return res;
 }
 
 void recognitionTest(char separator = ' ')
@@ -115,27 +128,80 @@ void recognitionTest(char separator = ' ')
         stringstream liness(line);
         getline(liness, path, separator);
         getline(liness, classlabel);
-        if (!path.empty() && !classlabel.empty())
-        {
-            int res = model->predict(imread("/home/sujith/ClionProjects/Comma/res/images/OUT/" + path, 0));
+        if (!path.empty() && !classlabel.empty()) {
+            int res = -1;
+            double conf = -1;
+            model->predict(imread("/home/sujith/ClionProjects/Comma/res/images/OUT/" + path, 0), res, conf);
+            cout << res << " " << conf << " " << labels[i];
             if (res == labels[i])
+            {
                 t++;
+                cout<<endl;
+            }
             else
+            {
                 f++;
-            cout << res << " " << labels[i] << endl;
+                cout<<" ** "<<endl;
+            }
+
         }
     }
     cout<<(100*t/(t+f))<<"%\n";
 }
 
+Mat preProcess(Mat image)
+{
+    Point2d e1,e2;
+    vector<Rect> eyes;
+    Mat img = Mat::zeros(image.rows,image.cols,image.type());
+    eye_detect.detectMultiScale(image,eyes);
+    dlib::full_object_detection shape;
+    dlib::shape_predictor sp;
+    shape = sp(dlib::cv_image<uchar>(image),dlib::rectangle(0,0,image.rows,image.cols));
+    cout<<shape.num_parts()<<endl;
+    cout<<"Hello\n"<<eyes.size()<<endl;
+    for(size_t i=0;i<2;i++)
+    {
+        cout<<eyes[i].x<<" "<<eyes[i].y<<" "<<eyes[i].width<<" "<<eyes[i].height<<endl;
+        rectangle(image,CvPoint(eyes[i].x,eyes[i].y),cvPoint(eyes[i].x+eyes[i].width,eyes[i].y+eyes[i].height),Scalar(0,0,250),2);
+    }
+    imshow("Origiinal",image);
+    waitKey(5000);
+    /*if(eyes.size()!=2)
+        return image;
+        */
+    e1.x = eyes[0].x+(eyes[0].width*0.5);
+    e1.y = eyes[0].y+(eyes[0].height*0.5);
+    e2.x = eyes[1].x+(eyes[1].width*0.5);
+    e2.y = eyes[1].y+(eyes[1].height*0.5);
+
+    if(e2.x<e1.x)
+    {
+        Point2d tmp = e2;
+        e2 = e1;
+        e1=tmp;
+    }
+    double angle = 90-atan2(e2.x-e1.x,e2.y-e1.y)*180/3.1415;
+    cout<<"Angle "<<angle<<endl;
+    warpAffine(image,img,getRotationMatrix2D(e1,angle,1),img.size());
+    imshow("Rotated",img);
+    waitKey(5000);
+    return img;
+
+}
+
+
 
 void fooDriver()
 {
-    //face_detect.load("./res/haarcascade_profileface.xml");
+    face_detect.load("/home/sujith/ClionProjects/Comma/res/haarcascade_profileface.xml");
+    eye_detect.load("/home/sujith/ClionProjects/Comma/res/haarcascade_eye.xml");
     //Mat image = imread("./res/images/AF01/AF01AFS.JPG");
     //imshow("IMAGE",image);
     //waitKey(0);
     //getFacesFromImage(image);
+    //recognitionTest();
     //train();
-    recognitionTest();
+    //cout<<recognize(imread("/home/sujith/ClionProjects/Comma/res/images/OUT/AF01-4.jpg"))<<endl;
+    preProcess(imread("/home/sujith/ClionProjects/Comma/res/images/OUT/BM01-6.jpg",0));
 }
--- FaceRecognizer-C/Generals.h
+++ FaceRecognizer-C/Generals.h
@@ -3,9 +3,15 @@
 //
 #include <iostream>
 #include <opencv2/opencv.hpp>
-
 #ifndef COMMA_GENERALS_H
 #define COMMA_GENERALS_H
+using namespace std;
+using namespace cv;
 void fooDriver();
-std::vector<cv::Mat> getFacesFromImage(cv::Mat);
+vector<Mat> getFacesFromImage(Mat);
+void readLines(const string&,vector<string>&);
+void prepareTrainData(string& , vector<Mat>& , vector<int>& , char);
+void train();
+void recognitionTest(char);
+Mat preProcess(Mat);
 #endif //COMMA_GENERALS_H
